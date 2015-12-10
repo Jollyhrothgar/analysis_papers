416f6a562ec1ab0c2816962ce1a699961cfea0af
Jollyhrothgar <michael.beaumier@gmail.com>
2015-12-10 13:10:00 -0800

    removed automation from vernier note, it was more trouble than it was worth. Added a template for my thesis.

--- run_12_vernier_analysis_note/vernier/main/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -fv main
-rm -fv main.pdf
---
+++ thesis/thesis/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rfv thesis.pdf
+rm -rfv thesis.toc
+rm -rfv thesis.pdfsync
+rm -rfv thesis.lot
+rm -rfv thesis.log
+rm -rfv thesis.lof
+rm -rfv thesis.fls
+rm -rfv thesis.dvi
+rm -rfv thesis.aux
+rm -rfv thesis.fdb_latexmk
+rm -rv thesis.bbl
+rm -rv thesis.blg
