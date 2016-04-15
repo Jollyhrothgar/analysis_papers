f66cf535497c26d38d4de60d5e3cf8dddd60ab8a
Jollyhrothgar <michael.beaumier@gmail.com>
2016-04-14 20:15:45 -0700

    fixed references and bibliography to take a direct mendeley import

--- my_thesis/open_thesis.sh
+++ my_thesis/open_thesis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-vim Abstract/abstract.tex Acknowlegements/acknowlegements.tex Chapter*/*.tex thesis/thesis.tex thesis/bibliography.bib thesis/packages.tex
+vim Abstract/abstract.tex Acknowlegements/acknowlegements.tex Chapter*/*.tex thesis/thesis.tex thesis/mendeley.bib thesis/packages.tex Appendix/appendix.tex
 
